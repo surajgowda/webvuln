2c541a19baa9117a9056a317a5a96a25f1e3e795
Suraj Gowda <surajnca1234@gmail.com>
2025-12-23 13:34:40 +0530

    deletion bugs

--- app.py
+++ app.py
@@ -75,11 +75,6 @@ def comments():
 def get_db():
     return sqlite3.connect(DB_NAME)
 
-def get_db_connection():
-    conn = sqlite3.connect(DB_NAME)
-    conn.row_factory = sqlite3.Row
-    return conn
-
 def delete_comments_job():
     print(f"[{datetime.now()}] Deleting comments...")
 
